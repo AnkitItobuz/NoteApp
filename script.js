e311247f1c03186d515c40585b229af73b5d2a6d
Ankit Kumar <ankit.kumar@itobuz.com>
2023-04-17 18:50:00 +0530

    added date

--- script.js
+++ script.js
@@ -7,29 +7,36 @@ const submit = document.getElementById("submit-button");
 
 submit.addEventListener("submit", function (e) {
   e.preventDefault();
+  var date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+
+  let currDate = `${day} - ${month} - ${year}`;
   const content = {
     title: input1.value,
-    text: input2.value,
+    content: input2.value,
+    date: currDate,
   };
   notesArr.push(content);
 
   showNotes();
 });
 
+
 function showNotes() {
   notesPreview.innerHTML = "";
 
   for (let a = 0; a < notesArr.length; a++) {
     const tag = document.createElement("div");
-    // const text = document.createTextNode(notesArr[a]);
+    let date = notesArr[a].date;
     let head = notesArr[a].title;
-    let content = notesArr[a].text;
-    tag.innerHTML = `<h1>${head}</h1> <p>${content}</p>`;
+    let content = notesArr[a].content;
 
-    // tag.innerHTML = notesArr.map(function(element){return `<h1>${element.title}</h1> <p>${element.text}</p>`});
+    tag.innerHTML = `<h4>${date}</h4> <h1>${head}</h1> <p>${content}</p>`;
 
-    // tag.append(text);
     notesPreview.appendChild(tag);
+
     tag.classList.add("box");
     input1.value = "";
     input2.value = "";
